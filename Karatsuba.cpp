7ed24231b97fd3dfc448fcb4f582424e927532d0
CheekyBreeky1 <azemilov@mail.ru>
2024-10-06 16:52:54 +0300

    Added Karatsuba and MergeSort

---
+++ Karatsuba.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+long long karatsuba(long long x, long long y)
+{
+	if (x < 10 or y < 10)
+		return x * y;
+
+	int len_x = to_string(x).length();
+	int len_y = to_string(y).length();
+
+	int divider = min(to_string(x).length(), to_string(y).length());
+	divider = divider / 2;
+
+	long a = x / pow(10, divider);
+	long b = x % (long)pow(10, divider);
+	long c = y / pow(10, divider);
+	long d = y % (long)pow(10, divider);
+
+	long long step_1 = karatsuba(a, c);
+	long long step_2 = karatsuba(b, d);
+	long long step_3 = karatsuba((a + b), (c + d));
+
+	long long step_4 = step_3 - step_2 - step_1;
+	long long result = step_1 * pow(10, 2*divider) + step_4 * pow(10,divider) + step_2;
+	return result;
+}
+
+void test_karatsuba(long long x,long long y,long long expect_result)
+{
+	if (karatsuba(x, y) == expect_result)
+	{cout << "OK\n"; }
+	else
+	{cout << "ERROR\n"; };
+
+}
+
+int main()
+{
+	test_karatsuba(4527, 8357, 37832139);
+	test_karatsuba(45279, 8357, 378396603);
+	test_karatsuba(683613, 89058, 60881206554);
+}
---
+++ MergeSort.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+void mergeSort(vector<int>& arr) {
+
+    if (arr.size() <= 1) {
+        return; 
+    }
+
+    int mid = arr.size() / 2;
+
+    vector<int> leftArr(arr.begin(), arr.begin() + mid);
+    vector<int> rightArr(arr.begin() + mid, arr.end());
+
+    mergeSort(leftArr);
+    mergeSort(rightArr);
+
+    int leftIndex = 0, rightIndex = 0, arrIndex = 0;
+
+    while (leftIndex < leftArr.size() and rightIndex < rightArr.size()) {
+        if (leftArr[leftIndex] <= rightArr[rightIndex]) {
+            arr[arrIndex++] = leftArr[leftIndex++];
+        }
+        else {
+            arr[arrIndex++] = rightArr[rightIndex++];
+        }
+    }
+
+    while (leftIndex < leftArr.size()) {
+        arr[arrIndex++] = leftArr[leftIndex++];
+    }
+
+    while (rightIndex < rightArr.size()) {
+        arr[arrIndex++] = rightArr[rightIndex++];
+    }
+}
+void testMergeSort() {
+    int size = 500;
+    vector<int> arr(size); 
+    srand(static_cast<unsigned>(time(0)));
+
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 1000;
+    }
+
+    cout << "arr before:\n";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+
+    mergeSort(arr);
+
+    cout << "arr after:\n";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+}
+
+int main()
+{
+    testMergeSort();
+}
